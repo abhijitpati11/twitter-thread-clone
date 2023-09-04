952b1024d31504cccc428f1eb3eaa5ecda0d15f9
Abhijit Pati <95014101+abhijitpati11@users.noreply.github.com>
2023-09-04 17:58:10 +0530

    The login page is re-designed

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,22 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TbNeedleThread } from 'react-icons/tb';
 import { FcGoogle } from 'react-icons/fc';
 import { signIn } from 'next-auth/react';
 
 const Login = () => {
+  
+  const [name, setName] = useState();
+  const [password, setPassword] = useState();
+
+  const handleLoginButton = () => {
+    setName("");
+    setPassword("");
+    alert("This feature is under development and will be available soon...Please use Google Authentication");
+  }
+
   return (
-    <div className='grid grid-cols-2'>
-      <div className='bg-[#606060] h-screen grid place-items-center'>
-        <TbNeedleThread className='text-white text-[230px]' />
-      </div>
-      <div className="grid place-items-center">
-       <div 
-        onClick={() => signIn('google')} 
-        className="flex gap4 bg-white p-4 px-6 
-                   items-center rounded-[7px] cursor-pointer">
-        <FcGoogle className='text-[35px]' />
-        <div>signIn With Google</div>
-       </div>
+    <div>
+      <div className='flex justify-center items-center h-screen bg-indigo-600'>
+        <div className='w-96 p-6 shadow-lg bg-white rounded-md'> 
+          <h1 className='text-3xl block text-center font-semibold'>
+            <i className='fa-solid fa-user'></i>
+            Login to Thread
+          </h1>
+          
+          <div className="mt-3">
+            <label htmlFor="userName" className='block text-base mb-2'>Username</label>
+            <input type="text" 
+              className='border w-full text-base px-2 py-1 focus:outline-none
+              focus:ring-0 focus:border-gray-600'
+              placeholder='Enter Username'
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+
+          <div className="mt-3">
+            <label htmlFor="password" className='block text-base mb-2'>Password</label>
+            <input type="password" 
+              className='border w-full text-base px-2 py-1 focus:outline-none
+              focus:ring-0 focus:border-gray-600'
+              placeholder='Enter Password' 
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="mt-3 flex justify-between items-center">
+            <div>
+              <input type="checkbox" />
+              <label htmlFor="label">Remember Me</label>
+            </div>
+            <div>
+              <a href="@" className='hover:text-red-500'> Forgot Password? </a>
+            </div>
+          </div>
+
+          <div className='mt-5'>
+            <button type='submit'
+              className='border-2 border-indigo-700 bg-indigo-700 text-white py-1 w-full rounded-md
+              hover:bg-transparent hover:text-indigo-700'
+              onClick={handleLoginButton}
+            >
+              &nbsp;&nbsp;Login
+            </button>
+          </div>
+
+          <div className='relative flex mt-4 items-center'>
+            <div className='flex-grow border-t-4 border-gray-400 ml-3'></div>
+            <span className='mx-3'>Or login With</span>
+            <div className='flex-grow border-t-4 border-gray-400 mr-3'></div>
+          </div>
+
+          <div className='mt-5'>
+            <button type='submit'
+              className='border-2 border-indigo-700 bg-transparent text-dark py-1 w-full rounded-lg
+              hover:bg-indigo-700 hover:text-white'
+              onClick={()=> signIn('google')}
+            > 
+              <div className='flex justify-center text-center' >
+                <span className='mt-1'><FcGoogle /></span>
+                <span>SignIn With Google</span>
+              </div>
+            </button>
+          </div>
+
+        </div>
       </div>
     </div>
   )
